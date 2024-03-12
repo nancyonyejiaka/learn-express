658c17a72e6f159724f48f5ee8742dc6ee0a79f1
Nancy Onyejiaka <nancyonyejiaka@Nancys-MacBook-Pro.local>
2024-03-12 16:07:41 -0400

    03/12: server4 quiz

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,20 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+
+router.get("/usernames", (req, res) => {
+  let usernames = req.users.map(function (user) {
+    return { id: user.id, username: user.username };
+  });
+  res.send(usernames);
+});
+
+router.get("/username/:username", (req, res) => {
+  let name = req.params.username;
+  let usersWithUsername = req.users.filter(function (user) {
+    return user.username === name;
+  });
+  console.log(usersWithUsername);
+  res.send(usersWithUsername);
+});
+
+module.exports = router;
